6ac6bb8b86835064373a38d3b919c2aad4756147
EgbuleNuel <egbulemmanuel@gmail.com>
2023-08-30 08:05:25 +0100

    updating commit

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import SearchIcon from '@mui/icons-material/Search';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
         <div className="left"></div>
+        <div className="item">
+          <img src="img/ngn.png" alt="" />
+          <KeyboardArrowDownIcon />
+        </div>
         <div className="center"></div>
         <div className="right"></div>
       </div>
